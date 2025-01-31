564838259b4a63f2c1894448f431b30e6264ad44
Manish Das <manishlakhaipur1999@gmail.com>
2025-01-31 08:44:08 +0530

    Add files via upload

---
+++ Arthmatical.js
@@ -0,0 +1,46 @@
+
+
+let a = 20;
+let b = 30;
+ let addition = a+b;
+console.log("this is addition number",addition);
+
+
+
+let c = 20;
+let d = 30;
+let subtraction = a-b;
+console.log("this is addition number",subtraction);
+
+
+
+let e = 20;
+let f = 30;
+ let multiplication = a*b;
+console.log("this is addition number",multiplication);
+
+
+
+let g = 20;
+let h = 30;
+ let division = a/b;
+console.log("this is addition number",division);
+
+
+
+let i = 20;
+let j = 30;
+ let modulo = a%b;
+console.log("this is addition number",modulo);
+
+
+
+
+let x = 20;
+let y = 30;
+
+x = x+y;
+y = x-y;
+x = x-y;
+console.log("x is value ",x)
+console.log("y is value ",y)
---
+++ Assiment.js
@@ -0,0 +1,37 @@
+
+let a = 10;
+a +=15;
+
+let addition = a;
+console.log(addition)
+
+let b = 15;
+b -=20;
+ let subtraction = b;
+console.log(subtraction)
+
+
+let c = 15;
+b *=20;
+ let multiplication = b;
+console.log(multiplication)
+
+
+let d = 15;
+b **=5;
+ let Exponentiation = b;
+console.log(Exponentiation)
+
+
+let e = 15;
+b /=20;
+ let division = b;
+console.log(division)
+
+
+
+
+let f = 15;
+b %=5;
+ let modulo = b;
+console.log(modulo)
